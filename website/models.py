e3c8ece62b96dd7f62083ff993f86726bb1954a9
Pooja <poojaspitel@gmail.com>
2026-05-07 16:11:52 +0530

    submit data to database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
--- website/views.py
+++ website/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
+from .models import Contact
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def services(request):
     return render(request, 'services.html')
 
+
 def contact(request):
+
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        phone = request.POST.get('phone')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            phone=phone
+        )
+
     return render(request, 'contact.html')
